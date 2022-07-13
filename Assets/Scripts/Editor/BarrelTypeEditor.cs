14a282235737fff72282b1261df40d3ef6ab20b9
Tom <60851185+TJJonker@users.noreply.github.com>
2022-07-14 00:10:55 +0200

    Update BarrelTypeEditor.cs

--- Assets/Scripts/Editor/BarrelTypeEditor.cs
+++ Assets/Scripts/Editor/BarrelTypeEditor.cs
@@ -12,13 +12,18 @@ public class BarrelTypeEditor : Editor {
 
         GUILayout.Label( "Bip Machine", EditorStyles.boldLabel );
 
-        using ( new GUILayout.HorizontalScope() ) {
-            GUILayout.Label( "Choose your sound:" );
-            sound = (Sounds) EditorGUILayout.EnumPopup( sound );
+        using ( new GUILayout.VerticalScope( EditorStyles.helpBox ) ) {
+            using ( new GUILayout.HorizontalScope() ) {
+                GUILayout.Label( "Sound:" );
+                sound = (Sounds) EditorGUILayout.EnumPopup( sound );
+            }
+
+            using(new GUILayout.HorizontalScope() ) {
+                GUILayout.Label( "Sound Volume:" );
+                soundVolume = GUILayout.HorizontalSlider( soundVolume, 0f, 8f, GUILayout.Height( 20 ) );
+            }
+
+            if ( GUILayout.Button( "Make Sound" ) ) Debug.Log( sound.ToString() );
         }
-
-        soundVolume = GUILayout.HorizontalSlider( soundVolume, 0f, 8f );
-
-        if ( GUILayout.Button( "Make Sound" ) ) Debug.Log( sound.ToString() );
     }
 }
