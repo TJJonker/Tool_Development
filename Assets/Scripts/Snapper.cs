27f2ab9ca9670cad0d9486ec91986a9aeb526efb
Tom <60851185+TJJonker@users.noreply.github.com>
2022-07-18 21:38:03 +0200

    Menu Item for snapping objects position

---
+++ Assets/Scripts/Snapper.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class Snapper {
+
+    private const string SNAP_TEXT = "Snap Objects";
+
+    [MenuItem( "Edit/Snap Selected GameObjects %&S", isValidateFunction: true )]
+    public static bool SnapObjectsValidate () {
+        return Selection.gameObjects.Length > 0;
+    }
+
+    [MenuItem( "Edit/Snap Selected GameObjects %&S" )]
+    public static void SnapObjects () {
+        foreach ( GameObject g in Selection.gameObjects ) {
+            Undo.RecordObject( g.transform, SNAP_TEXT );
+            g.transform.position = g.transform.position.Round();
+        }
+    }
+
+    public static Vector3 Round ( this Vector3 v ) {
+        v.x = Mathf.Round( v.x );
+        v.y = Mathf.Round( v.y );
+        v.z = Mathf.Round( v.z );
+        return v;
+    }
+}
