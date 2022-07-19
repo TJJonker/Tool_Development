52bcec483ee1940c8cbd31a62e8a73532d56a52f
Tom <60851185+TJJonker@users.noreply.github.com>
2022-07-19 17:06:58 +0200

    Snappertool contains a variable for gridSize

--- Assets/Scripts/Editor/Snapper.cs
+++ Assets/Scripts/Editor/Snapper.cs
@@ -17,11 +17,4 @@ public static class Snapper {
             g.transform.position = g.transform.position.Round();
         }
     }
-
-    public static Vector3 Round ( this Vector3 v ) {
-        v.x = Mathf.Round( v.x );
-        v.y = Mathf.Round( v.y );
-        v.z = Mathf.Round( v.z );
-        return v;
-    }
 }
--- Assets/Scripts/Editor/Snappertool.cs
+++ Assets/Scripts/Editor/Snappertool.cs
@@ -5,23 +5,46 @@ public class Snappertool : EditorWindow {
 
     private const string UNDO_SNAP_TEXT = "Snap Objects";
 
+    SerializedObject so;
+    public float gridSize = 1f;
+    SerializedProperty propGridSize;
+
+
+
     [MenuItem( "Tools / Snapper %&S" )]
     public static void OpenWindow () => GetWindow<Snappertool>( "Snapper" );
 
 
-    private void OnEnable () => Selection.selectionChanged += Repaint;
-    private void OnDisable () => Selection.selectionChanged -= Repaint;
+    private void OnEnable () {
+        so = new SerializedObject( this );
+        propGridSize = so.FindProperty( "gridSize" );
 
+        Selection.selectionChanged += Repaint;
+        SceneView.duringSceneGui += DuringSceneGUI;
+    }
+
+    private void OnDisable () {
+        Selection.selectionChanged -= Repaint;
+        SceneView.duringSceneGui -= DuringSceneGUI;
+    }
 
     private void OnGUI () {
+        so.Update();
+        EditorGUILayout.PropertyField( propGridSize );
+        so.ApplyModifiedProperties();
+
         using ( new EditorGUI.DisabledScope( Selection.gameObjects.Length == 0 ) )
             if ( GUILayout.Button( "Snap Selection" ) ) Snap();
+    }
+
+    private void DuringSceneGUI(SceneView view ) {
+
     }
 
     private void Snap () {
         foreach ( GameObject g in Selection.gameObjects ) {
             Undo.RecordObject( g.transform, UNDO_SNAP_TEXT );
-            g.transform.position = g.transform.position.Round();
+            g.transform.position = g.transform.position.Round( gridSize );
         }
     }
 }
