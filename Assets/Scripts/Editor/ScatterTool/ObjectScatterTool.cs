a194ae8492a26c852a2ef2dff8ff7271c01c3409
Tom <60851185+TJJonker@users.noreply.github.com>
2022-08-02 17:04:04 +0200

    Feat/Chore (ObjectScatterTool) : Prefab spawning and partial rewrite

--- Assets/Scripts/Editor/ScatterTool/ObjectScatterTool.cs
+++ Assets/Scripts/Editor/ScatterTool/ObjectScatterTool.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -21,30 +22,33 @@ public class ObjectScatterTool : EditorWindow {
     [SerializeField] private float range;
     SerializedProperty propAmount;
     [SerializeField] private int amount;
+    SerializedProperty propPrefab;
+    [SerializeField] private GameObject prefab;
 
     private Vector2[] generatedPoints;
-    private Vector3[] positions;
 
 
     private void OnEnable () {
         so = new SerializedObject( this );
         propRange = so.FindProperty( "range" );
         propAmount = so.FindProperty( "amount" );
+        propPrefab = so.FindProperty( "prefab" );
 
         range = EditorPrefs.GetFloat( RANGE_SAVE_STRING, MINIMUM_RANGE_VALUE );
         amount = EditorPrefs.GetInt( AMOUNT_SAVE_STRING, MINIMUM_AMOUNT_VALUE );
 
-        GenerateRandomPoints();
+        GenerateRandomPoints( amount );
 
         SceneView.duringSceneGui += DuringSceneGUI;
     }
+
 
     private void OnDisable () {
 
         EditorPrefs.SetFloat( RANGE_SAVE_STRING, range );
         EditorPrefs.SetInt( AMOUNT_SAVE_STRING, amount );
 
-        SceneView.duringSceneGui += DuringSceneGUI;
+        SceneView.duringSceneGui -= DuringSceneGUI;
     }
 
     private void OnGUI () {
@@ -56,8 +60,10 @@ public class ObjectScatterTool : EditorWindow {
         EditorGUILayout.PropertyField( propAmount );
         propAmount.intValue = propAmount.intValue.Between( MINIMUM_AMOUNT_VALUE, MAXIMUM_AMOUNT_VALUE );
 
+        EditorGUILayout.PropertyField( propPrefab );
+
         if ( so.ApplyModifiedProperties() ) {
-            GenerateRandomPoints();
+            GenerateRandomPoints( amount );
             SceneView.RepaintAll();
         }
 
@@ -67,8 +73,18 @@ public class ObjectScatterTool : EditorWindow {
         }
     }
 
+
+
     private void DuringSceneGUI ( SceneView sceneView ) {
-        if ( Event.current.type == EventType.MouseMove ) sceneView.Repaint();
+
+        // Calculate hipoint and tangent space
+        var cameraTransform = sceneView.camera.transform;
+        Ray ray = HandleUtility.GUIPointToWorldRay( Event.current.mousePosition );
+        Physics.Raycast( ray, out RaycastHit hit );
+
+        var tangent = Vector3.Cross( hit.normal, cameraTransform.up ).normalized;
+        var biTangent = Vector3.Cross( hit.normal, tangent );
+
 
 
         bool ctrlHeld = ( Event.current.modifiers & EventModifiers.Control ) != 0;
@@ -82,58 +98,74 @@ public class ObjectScatterTool : EditorWindow {
             Event.current.Use();
         }
 
-        if ( Event.current.type != EventType.Repaint ) return;
 
-        var cameraTransform = sceneView.camera.transform;
-        Ray ray = HandleUtility.GUIPointToWorldRay( Event.current.mousePosition );
-        //Ray ray = new Ray( cameraTransform.position, cameraTransform.forward );
-        Physics.Raycast( ray, out RaycastHit hit );
+        List<RaycastHit> rayCastHits = new List<RaycastHit>();
+        foreach ( Vector2 p in generatedPoints )
+            if ( Physics.Raycast( GetTangentRay( p ), out RaycastHit h ) )
+                rayCastHits.Add( h );
 
-        var tangent = Vector3.Cross( hit.normal, cameraTransform.up ).normalized;
-        var biTangent = Vector3.Cross( hit.normal, tangent );
-
-        Handles.zTest = CompareFunction.LessEqual;
-        Handles.color = Color.blue;
-        Handles.DrawAAPolyLine( 5f, hit.point, hit.point + hit.normal );
-        Handles.color = Color.red;
-        Handles.DrawAAPolyLine( 5f, hit.point, hit.point + tangent );
-        Handles.color = Color.green;
-        Handles.DrawAAPolyLine( 5f, hit.point, hit.point + biTangent );
-
-        const int circleDetail = 64;
-        Vector3[] circlePosition = new Vector3[ circleDetail ];
-        for ( int i = 0; i < circleDetail; i++ ) {
-            // Split the circle up in equal parts/angles
-            var p = 360f / circleDetail * i;
-            var radP = p * Mathf.Deg2Rad;
-            // Calculate the positions from the calculated angle
-            Ray r = GetTangentRay( new Vector2( Mathf.Cos( radP ), Mathf.Sin( radP ) ) );
-            // Save position in an array
-            if ( Physics.Raycast( r, out RaycastHit h ) ) circlePosition[ i ] = h.point;
-            else circlePosition[ i ] = r.origin;
+        if ( ctrlHeld && Event.current.type == EventType.MouseDown ) {
+            foreach ( RaycastHit h in rayCastHits ) {
+                GameObject spawnedObject = (GameObject) PrefabUtility.InstantiatePrefab( prefab );
+                Undo.RegisterCreatedObjectUndo( spawnedObject, "Spawn Objects" );
+                spawnedObject.transform.position = h.point;
+                spawnedObject.transform.rotation = Quaternion.LookRotation( h.normal ) * Quaternion.Euler( 90f, 0, 0 );
+            }
         }
 
-        Handles.color = Color.blue;
-        Handles.DrawAAPolyLine( circlePosition );
+
+        if ( Event.current.type == EventType.MouseMove ) sceneView.Repaint();
+        if ( Event.current.type == EventType.Repaint ) {
+
+            #region Draw origin point and Tangent Space Gizmo
+
+            Handles.zTest = CompareFunction.LessEqual;
+            Handles.color = Color.blue;
+            Handles.DrawAAPolyLine( 5f, hit.point, hit.point + hit.normal );
+            Handles.color = Color.red;
+            Handles.DrawAAPolyLine( 5f, hit.point, hit.point + tangent );
+            Handles.color = Color.green;
+            Handles.DrawAAPolyLine( 5f, hit.point, hit.point + biTangent );
+            #endregion
+
+            #region Draw detailed circle which adapts to the surface
+            const int circleDetail = 64;
+            Vector3[] circlePosition = new Vector3[ circleDetail ];
+            for ( int i = 0; i < circleDetail; i++ ) {
+                // Split the circle up in equal parts/angles
+                var p = 360f / circleDetail * i;
+                var radP = p * Mathf.Deg2Rad;
+                // Calculate the positions from the calculated angle
+                Ray r = GetTangentRay( new Vector2( Mathf.Cos( radP ), Mathf.Sin( radP ) ) );
+                // Save position in an array
+                if ( Physics.Raycast( r, out RaycastHit h ) ) circlePosition[ i ] = h.point;
+                else circlePosition[ i ] = r.origin;
+            }
+
+            Handles.color = Color.blue;
+            Handles.DrawAAPolyLine( circlePosition );
+            #endregion
+
+            #region Draw generated points and their normals
+            Handles.color = Color.white;
+            foreach ( RaycastHit hp in rayCastHits ) {
+                Handles.DrawAAPolyLine( hp.point, hp.point + hp.normal );
+                Handles.DrawSolidDisc( hp.point, hp.normal, .1f );
+            }
+            #endregion
+        }
 
         Ray GetTangentRay ( Vector2 pointPosition ) {
             var originalPos = hit.point + ( tangent * pointPosition.x + biTangent * pointPosition.y ) * range;
             return new Ray( originalPos + hit.normal * 3, -hit.normal );
         }
-
-        Handles.color = Color.white;
-        foreach ( Vector2 p in generatedPoints ) {
-            if ( Physics.Raycast( GetTangentRay( p ), out RaycastHit newPosHit ) ) {
-                Handles.DrawAAPolyLine( newPosHit.point, newPosHit.point + newPosHit.normal );
-                Handles.DrawSolidDisc( newPosHit.point, newPosHit.normal, .1f );
-            }
-        }
     }
 
-    private void GenerateRandomPoints () {
-        generatedPoints = new Vector2[ amount ];
-        for ( int i = 0; i < amount; i++ )
+    private void GenerateRandomPoints ( int amountOfPoints ) {
+        generatedPoints = new Vector2[amountOfPoints];
+        for ( int i = 0; i < amountOfPoints; i++ ) {
             generatedPoints[ i ] = Random.insideUnitCircle;
+        }
     }
 
 }
